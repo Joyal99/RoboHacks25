fed69a3c298c3bcb9a034f294751c979e1e8534c
Aaida Hossain <aaida.hossain1@gmail.com>
2025-02-15 02:09:44 -0500

    Update IRtest.ino

--- Sensors/IR_Sensor/IRtest.ino
+++ Sensors/IR_Sensor/IRtest.ino
@@ -1,20 +1,29 @@
-#define MS 12      // Middle Sensor
+#define LEFT_SENSOR 12
+#define RIGHT_SENSOR 3
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(MS, INPUT);  // Set the Middle Sensor as input
+// Function to initialize the setup
+void setupSensors() {
+    Serial.begin(9600);
+    pinMode(LEFT_SENSOR, INPUT);
+    pinMode(RIGHT_SENSOR, INPUT);
 }
 
-void loop() {
-  // Read the Middle Sensor value
-  int sensorValue = digitalRead(MS);
+// Function to read the left sensor value
+int readLeftSensor() {
+    int leftValue = digitalRead(LEFT_SENSOR);
+    Serial.println(leftValue);
+    delay(10);
+    return leftValue;
+}
 
-  // Check if the sensor reads HIGH or LOW
-  if (sensorValue == HIGH) {
-    Serial.println("Sensor is HIGH");
-  } else {
-    Serial.println("Sensor is LOW");
-  }
+// Function to read the right sensor value
+int readRightSensor() {
+    int rightValue = digitalRead(RIGHT_SENSOR);
+    Serial.println(rightValue);
+    delay(10);
+    return rightValue;
+}
 
-  delay(100);  // Add a small delay for stability
+void setup() {
+    setupSensors();
 }
