61d0500bd953de6a4af12527a66de5049cd090e4
Arnaud Denis-Remillard <108596859+myrodar@users.noreply.github.com>
2025-02-15 11:53:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,28 +3,35 @@
 #include <avr/io.h>
 
 //-------------------- Motor Control Pins --------------------
-#define LEFTM_FOWARD_PIN 4    
-#define LEFTM_BACK_PIN   5    
-#define RIGHTM_FOWARD_PIN 6   
-#define RIGHTM_BACK_PIN  7    
-#define RIGHTM_EN_PIN    3    
-#define LEFTM_EN_PIN     11   
+#define LEFTM_FOWARD_PIN  4
+#define LEFTM_BACK_PIN    5
+#define RIGHTM_FOWARD_PIN 6
+#define RIGHTM_BACK_PIN   7
+#define RIGHTM_EN_PIN     3
+#define LEFTM_EN_PIN      11
 
-#define DEFAULT_SPEED 85     
+#define DEFAULT_SPEED     100
 
 //-------------------- IR Sensor Pins --------------------
-#define RIGHT_SENSOR 12       
-#define LEFT_SENSOR 8   
+#define RIGHT_SENSOR 12
+#define LEFT_SENSOR  8
 
-//--------------------- Color Sensor Pins ---------------
-volatile uint16_t pulse_count = 0;
-#define CS_S0_PIN PC0  // A0
-#define CS_S1_PIN PC1  // A1
-#define CS_S2_PIN PC2  // A2
-#define CS_S3_PIN PC3  // A3
-#define CS_OUT_PIN PD2  // Use PD2 for INT0
-#define CS_LED_PIN PB5  // LED control pin
-volatile char prevChar = 'U'; // Tracks the previously detected color
+//-------------------- Color Sensor Pins --------------------
+volatile uint16_t pulse_count = 0;    // Incremented by ISR
+#define CS_S0_PIN   PC0  // A0
+#define CS_S1_PIN   PC1  // A1
+#define CS_S2_PIN   PC2  // A2
+#define CS_S3_PIN   PC3  // A3
+#define CS_OUT_PIN  PD2  // Use PD2 for INT0
+#define CS_LED_PIN  PB5  // LED control pin
+
+// Tracks previously detected color so we don't re-trigger the same event repeatedly
+volatile char prevChar = 'U';
+
+//-------------------- Timing Variables --------------------
+// We'll check color once every 300 ms (adjust as needed)
+unsigned long lastColorCheck = 0;
+const unsigned long colorCheckInterval = 300;  // ms
 
 //--------------------------------------------
 // Motor Functions
@@ -39,19 +46,19 @@ void motorPinSetup() {
 }
 
 void moveForward(uint8_t speed) {
-  digitalWrite(LEFTM_FOWARD_PIN, HIGH);
-  digitalWrite(LEFTM_BACK_PIN,   LOW);
+  digitalWrite(LEFTM_FOWARD_PIN,  HIGH);
+  digitalWrite(LEFTM_BACK_PIN,    LOW);
   digitalWrite(RIGHTM_FOWARD_PIN, HIGH);
-  digitalWrite(RIGHTM_BACK_PIN,  LOW);
+  digitalWrite(RIGHTM_BACK_PIN,   LOW);
   analogWrite(LEFTM_EN_PIN, speed);
-  analogWrite(RIGHTM_EN_PIN, speed+10);
+  analogWrite(RIGHTM_EN_PIN, speed);
 }
 
 void turnLeft(uint8_t speed) {
   digitalWrite(RIGHTM_FOWARD_PIN, HIGH);
   digitalWrite(RIGHTM_BACK_PIN,   LOW);
-  digitalWrite(LEFTM_FOWARD_PIN, LOW);
-  digitalWrite(LEFTM_BACK_PIN,   HIGH);
+  digitalWrite(LEFTM_FOWARD_PIN,  LOW);
+  digitalWrite(LEFTM_BACK_PIN,    HIGH);
   analogWrite(LEFTM_EN_PIN, speed);
   analogWrite(RIGHTM_EN_PIN, speed);
 }
@@ -59,94 +66,64 @@ void turnLeft(uint8_t speed) {
 void turnRight(uint8_t speed) {
   digitalWrite(RIGHTM_FOWARD_PIN, LOW);
   digitalWrite(RIGHTM_BACK_PIN,   HIGH);
-  digitalWrite(LEFTM_FOWARD_PIN, HIGH);
-  digitalWrite(LEFTM_BACK_PIN,   LOW);
+  digitalWrite(LEFTM_FOWARD_PIN,  HIGH);
+  digitalWrite(LEFTM_BACK_PIN,    LOW);
   analogWrite(LEFTM_EN_PIN, speed);
-  analogWrite(RIGHTM_EN_PIN, speed+10);
+  analogWrite(RIGHTM_EN_PIN, speed);
 }
 
 void stopMotors() {
   analogWrite(LEFTM_EN_PIN,  0);
   analogWrite(RIGHTM_EN_PIN, 0);
-  digitalWrite(LEFTM_FOWARD_PIN, LOW);
-  digitalWrite(LEFTM_BACK_PIN,   LOW);
+  digitalWrite(LEFTM_FOWARD_PIN,  LOW);
+  digitalWrite(LEFTM_BACK_PIN,    LOW);
   digitalWrite(RIGHTM_FOWARD_PIN, LOW);
-  digitalWrite(RIGHTM_BACK_PIN,  LOW);
+  digitalWrite(RIGHTM_BACK_PIN,   LOW);
 }
 
-//-------------------------------------------
+//--------------------------------------------
 // Color Sensor
-//-------------------------------------------
-char getColor() {
-  enableSensorLED();  // Turn on the sensor LED
-  _delay_ms(5);       // Stabilization time
-
-  uint16_t red = measureColorFreq('R');
-  uint16_t green = measureColorFreq('G');
-  uint16_t blue = measureColorFreq('B');
-
-  // Apply calibration
-  red = calibrateColor(red, 'R');
-  green = calibrateColor(green, 'G');
-  blue = calibrateColor(blue, 'B');
-
-  char col;
-
-  // Improved color detection thresholds and logic
-  if (red < 150 && green < 150 && blue < 150) {
-    col = 'L'; // Black or low light
-  } else if (green > 2000 && red > 2000 && blue > 2000) {
-    col = 'W'; // White
-  } else if (green > red && green > blue) {
-    col = 'G'; // Green
-  } else if (red > green && red > blue) {
-    col = 'R'; // Red
-  } else if (blue > red && blue > green) {
-    col = 'B'; // Blue
-  } else {
-    col = 'U'; // Unknown
-  }
-
-  disableSensorLED();  // Turn off the sensor LED
-  return col;
-}
-
+//--------------------------------------------
 ISR(INT0_vect) {
   pulse_count++;
 }
 
 void pinSetupCS() {
-  ADCSRA &= ~(1 << ADEN); // Disable ADC
+  // Disable ADC to use A0-A3 as digital
+  ADCSRA &= ~(1 << ADEN);
+
   // Set S0-S3 and LED pin as outputs
   DDRC |= (1 << CS_S0_PIN) | (1 << CS_S1_PIN) | (1 << CS_S2_PIN) | (1 << CS_S3_PIN);
-  // Set frequency scaling to 20%
-  PORTC |= (1 << CS_S0_PIN);    // S0 high
-  PORTC &= ~(1 << CS_S1_PIN);   // S1 low
+  // Frequency scaling to 20% (S0=HIGH, S1=LOW)
+  PORTC |= (1 << CS_S0_PIN);
+  PORTC &= ~(1 << CS_S1_PIN);
+
   // Set OUT pin as input with pull-up
   DDRD &= ~(1 << CS_OUT_PIN);
   PORTD |= (1 << CS_OUT_PIN);
+
   // Set up LED pin
   DDRB |= (1 << CS_LED_PIN);
 }
 
 void interruptSetupCS() {
-  EIMSK |= (1 << INT0);     // Enable INT0
-  EICRA |= (1 << ISC00);    // Trigger on any edge
-  sei();                    // Enable global interrupts
+  EIMSK |= (1 << INT0);    // Enable INT0
+  EICRA |= (1 << ISC00);   // Trigger on any edge
+  sei();                   // Enable global interrupts
 }
 
 void colorSel(char color) {
   switch (color) {
-    case 'R':
-      PORTC &= ~((1 << CS_S2_PIN) | (1 << CS_S3_PIN));  // S2=0, S3=0
+    case 'R': // Red filter => S2=0, S3=0
+      PORTC &= ~((1 << CS_S2_PIN) | (1 << CS_S3_PIN));
       break;
-    case 'G':
-      PORTC |= (1 << CS_S2_PIN);     // S2=1
-      PORTC |= (1 << CS_S3_PIN);     // S3=1
+    case 'G': // Green filter => S2=1, S3=1
+      PORTC |= (1 << CS_S2_PIN);
+      PORTC |= (1 << CS_S3_PIN);
       break;
-    case 'B':
-      PORTC &= ~(1 << CS_S2_PIN);    // S2=0
-      PORTC |= (1 << CS_S3_PIN);     // S3=1
+    case 'B': // Blue filter => S2=0, S3=1
+      PORTC &= ~(1 << CS_S2_PIN);
+      PORTC |=  (1 << CS_S3_PIN);
       break;
   }
 }
@@ -159,37 +136,99 @@ void disableSensorLED() {
   PORTB &= ~(1 << CS_LED_PIN);
 }
 
+/**
+ * measureColorFreq():
+ *  1) Select the filter (R/G/B)
+ *  2) Wait briefly for settling
+ *  3) Count pulses for a short time
+ *
+ * Shorter times => less blocking but also less accurate.
+ */
 uint16_t measureColorFreq(char color) {
   pulse_count = 0;
   colorSel(color);
-  _delay_ms(50);  // Allow filter to settle
-  pulse_count = 0;  // Reset after settling
-  _delay_ms(100);  // Measure for 100ms
+
+  // Shorter settle time
+  _delay_ms(10);
+
+  // Count pulses for a short measurement window
+  pulse_count = 0;
+  _delay_ms(20);
+
   return pulse_count;
 }
 
+/**
+ * Simple calibration function:
+ *  - Subtracts a minimum offset
+ *  - Applies a scaling factor
+ */
 uint16_t calibrateColor(uint16_t rawValue, char color) {
-  // Adjusted calibration values based on typical sensor response
-  const uint16_t minValues[] = {30, 30, 25};  // Minimum values for R, G, B
-  const float scalingFactors[] = {1.0, 1.1, 0.9};  // Scaling factors for R, G, B
+  const uint16_t minValues[] = {30, 30, 25};     // R, G, B offsets
+  const float scalingFactors[] = {1.0, 1.1, 0.9}; // R, G, B scale
 
   uint8_t colorIndex;
   switch (color) {
     case 'R': colorIndex = 0; break;
     case 'G': colorIndex = 1; break;
     case 'B': colorIndex = 2; break;
-    default: return rawValue;
+    default:  return rawValue;
   }
 
   if (rawValue <= minValues[colorIndex]) return 0;
   return (uint16_t)((rawValue - minValues[colorIndex]) * scalingFactors[colorIndex]);
 }
 
+/**
+ * getColor():
+ *  - Turns on sensor LED
+ *  - Measures freq for R, G, B
+ *  - Applies thresholds to decide color
+ */
+char getColor() {
+  enableSensorLED();
+  _delay_ms(5); // small LED stabilization
+
+  uint16_t red   = measureColorFreq('R');
+  uint16_t green = measureColorFreq('G');
+  uint16_t blue  = measureColorFreq('B');
+
+  // Calibrate each
+  red   = calibrateColor(red,   'R');
+  green = calibrateColor(green, 'G');
+  blue  = calibrateColor(blue,  'B');
+
+  char col;
+
+  // Basic color detection logic
+  if (red < 150 && green < 150 && blue < 150) {
+    col = 'L'; // Possibly black/low light
+  }
+  else if (green > 2000 && red > 2000 && blue > 2000) {
+    col = 'W'; // White
+  }
+  else if (green > red && green > blue) {
+    col = 'G'; // Green
+  }
+  else if (red > green && red > blue) {
+    col = 'R'; // Red
+  }
+  else if (blue > red && blue > green) {
+    col = 'B'; // Blue
+  }
+  else {
+    col = 'U'; // Unknown
+  }
+
+  disableSensorLED();
+  return col;
+}
+
 //--------------------------------------------
 // IR Sensor Functions
 //--------------------------------------------
 void setupSensors() {
-  pinMode(LEFT_SENSOR, INPUT);
+  pinMode(LEFT_SENSOR,  INPUT);
   pinMode(RIGHT_SENSOR, INPUT);
 }
 
@@ -201,62 +240,73 @@ int readRightSensor() {
   return digitalRead(RIGHT_SENSOR);
 }
 
-void followLine(){
-    int leftVal = readLeftSensor();
-    int rightVal = readRightSensor();
+/**
+ * followLine():
+ *  - Reads IR sensors
+ *  - Adjusts motor signals accordingly
+ */
+void followLine() {
+  int leftVal  = readLeftSensor();
+  int rightVal = readRightSensor();
 
-    // Line-following logic
-    if (leftVal == LOW && rightVal == HIGH) {
-      turnRight(DEFAULT_SPEED);
-    } else if (leftVal == HIGH && rightVal == LOW) {
-      turnLeft(DEFAULT_SPEED);
-    } else if (leftVal == LOW && rightVal == LOW) {
-      moveForward(DEFAULT_SPEED);
-    } else if (leftVal == HIGH && rightVal == HIGH) {
-      turnLeft(DEFAULT_SPEED / 2);
-    }
+  // Basic line-follow logic
+  if (leftVal == LOW && rightVal == HIGH) {
+    turnRight(DEFAULT_SPEED);
+  } 
+  else if (leftVal == HIGH && rightVal == LOW) {
+    turnLeft(DEFAULT_SPEED);
+  } 
+  else if (leftVal == LOW && rightVal == LOW) {
+    moveForward(DEFAULT_SPEED);
+  } 
+  else if (leftVal == HIGH && rightVal == HIGH) {
+    // Both off the line => mild turn
+    turnLeft(DEFAULT_SPEED / 2);
+  }
 }
 
 //--------------------------------------------
-// Main Function for Line Following
+// Arduino Setup & Loop
 //--------------------------------------------
-int main() {
-  init();
+void setup() {
   Serial.begin(9600);
-  delay(1000);
 
   pinSetupCS();
   motorPinSetup();
   setupSensors();
   interruptSetupCS();
+}
 
-  while (1) {
-    // Read IR sensor values
-    
+/**
+ * loop():
+ *  - Continuously follow line (fast)
+ *  - Check color every 'colorCheckInterval' ms
+ *  - If new color is G/B, stop for 1s or 8s
+ */
+void loop() {
+  // 1) Continuously follow line
+  followLine();
 
-    // Check for color detection periodically
+  // 2) Periodically check color
+  if (millis() - lastColorCheck >= colorCheckInterval) {
+    lastColorCheck = millis();
+
     char col = getColor();
 
-    // Process color detection using if-else statements
-    if (col == 'W') {
-      // If white is detected, continue line following (no action needed)
-      continue;
-    } else if (col == 'G' && prevChar != 'G') {
-      // If green is detected and it's a new detection
-      stopMotors();      // Stop the motors
-      _delay_ms(1000);   // Wait for 1 second
-      prevChar = 'G';    // Update the previous color
-    } else if (col == 'B' && prevChar != 'B') {
-      // If blue is detected and it's a new detection
-      stopMotors();      // Stop the motors
-      _delay_ms(8000);   // Wait for 8 seconds
-      prevChar = 'B';    // Update the previous color
+    // If we detect green (G) and it's a new detection
+    if (col == 'G' && prevChar != 'G') {
+      stopMotors();
+      delay(1000);     // 1 second stop
+      prevChar = 'G';
+    } 
+    // If we detect blue (B) and it's a new detection
+    else if (col == 'B' && prevChar != 'B') {
+      stopMotors();
+      delay(8000);     // 8 second stop
+      prevChar = 'B';
     }
-
-    // Resume line following after color detection
-    followLine();
-    _delay_ms(5);// Move forward at default speed
+    // White, black, or red? Just continue line following
   }
 
-  return 0;
+  // The loop repeats quickly, so the car remains responsive.
 }
