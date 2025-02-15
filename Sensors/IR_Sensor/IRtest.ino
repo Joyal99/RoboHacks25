7a99438a7284e37e465e54175843f2176368c301
Aaida Hossain <aaida.hossain1@gmail.com>
2025-02-15 02:11:56 -0500

    Update IRtest.ino

--- Sensors/IR_Sensor/IRtest.ino
+++ Sensors/IR_Sensor/IRtest.ino
@@ -1,25 +1,13 @@
-#define LEFT_SENSOR 12
-#define RIGHT_SENSOR 3
+#define LEFT_SENSOR 7
 
-// Function to initialize the setup
-void setupSensors() {
-    Serial.begin(9600);
-    pinMode(LEFT_SENSOR, INPUT);
-    pinMode(RIGHT_SENSOR, INPUT);
+void setup(){
+Serial.begin(9600);
+pinMode(LEFT_SENSOR,INPUT);
 }
 
-// Function to read the left sensor value
-int readLeftSensor() {
-    int leftValue = digitalRead(LEFT_SENSOR);
-    Serial.println(leftValue);
-    delay(10);
-    return leftValue;
+void loop(){
+  int leftValue = digitalRead(LEFT_SENSOR);
+  Serial.println(leftValue);
+  delay(10);
 }
 
-// Function to read the right sensor value
-int readRightSensor() {
-    int rightValue = digitalRead(RIGHT_SENSOR);
-    Serial.println(rightValue);
-    delay(10);
-    return rightValue;
-}
